86962eb55661d7f9d9f27a101c330e1b10bae015
chris westerduin <chriswesterduin@outlook.com>
2022-02-19 13:01:10 +0000

    initial commit

---
+++ src/main/java/com/mountainspring/MountainSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mountainspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountainSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mountainspring/MountainSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountainspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
